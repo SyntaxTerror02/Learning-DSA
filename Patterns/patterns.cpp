284ad71a1556d5c1c0392978af7a4a6d328c6ba2
Aditya Pandit <adityawork2203@gmail.com>
2025-07-28 20:05:15 +0530

    Add files via upload

---
+++ Patterns/patterns.cpp
@@ -0,0 +1,122 @@
+#include<iostream>
+
+using namespace std;
+
+void pattern1(int n){
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+    {
+        cout << "* ";
+    }
+    cout << endl;
+    }
+}
+
+void pattern2(int n){
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+    {
+        cout << "* ";
+    }
+    cout << endl;
+    }
+}
+
+void pattern3(int n){
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+    {
+        cout << j << " ";  //printing 1 to row no.
+    }
+    cout << endl;
+    }
+}
+
+void pattern4(int n){
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+    {
+        cout << i << " ";  
+    }
+    cout << endl;
+    }
+}
+
+void pattern5(int n){
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n; j >= i; j--)
+    {
+        cout << "* ";  
+    }
+    cout << endl;
+    }
+}
+
+void pattern5_1(int n){
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n-i+1; j++)
+    {
+        cout << "* ";  
+    }
+    cout << endl;
+    }
+}
+
+void pattern6(int n){
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n; j >= i; j--)
+    {
+        cout << j << " ";  
+    }
+    cout << endl;
+    }
+}
+
+void pattern6_1(int n){
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n-i+1; j++)
+    {
+        cout << j << " ";  
+    }
+    cout << endl;
+    }
+}
+
+void pattern7(int n){
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n; j >= i; j--)
+    {
+        cout << j << " ";  
+    }
+    cout << endl;
+    }
+}
+
+
+int main(){
+
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    pattern6(n);
+
+    return 0;
+}
